28a663ea47f7dc074bbd90e8b0564ede5ee7c3db
Nishikant00 <githubnsr@gmail.com>
2024-06-25 23:57:59 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn,address=server_socket.accept() # wait for client
+    bytes,addr=conn.recv()
+    print(bytes)
+    if bytes:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 if __name__ == "__main__":
     main()
