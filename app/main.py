8f3c66a08ba22529a12b150addde5a15e25c30f6
Nishikant00 <githubnsr@gmail.com>
2024-06-28 00:20:00 +0530

    Multithreading

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
-
 import socket
+import threading
 
-
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn,address=server_socket.accept() # wait for client
+def connection_handler(conn,address):
     data=conn.recv(4096).decode().split('\r\n')
     request=data[0].split(' ')
-    response=f'HTTP/1.1 404 Not Found\r\n\r\n'.encode()
+    response=f'HTTP/1.1 404 Not Found\r\n\r\n'
     if request[1]=='/':
-        response=f'HTTP/1.1 200 OK\r\n\r\n'.encode()
+        response=f'HTTP/1.1 200 OK\r\n\r\n'
     elif 'echo' in request[1]:
         endpoint=request[1].split('/')[2]
-        response=f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(endpoint)}\r\n\r\n{endpoint}'.encode()
+        response=f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(endpoint)}\r\n\r\n{endpoint}'
     elif 'user-agent' in request[1]:
-        response=f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data[2].split(' ')[1])}\r\n\r\n{data[2].split(' ')[1]}'.encode()
-    conn.sendall(response)
+        response=f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data[2].split(' ')[1])}\r\n\r\n{data[2].split(' ')[1]}'
+    conn.sendall(response.encode())
+    conn.close()
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    try:
+        while True:
+            conn,address=server_socket.accept() 
+            threading.Thread(target=connection_handler, args=(conn, address)).start()
+    except:
+        print('Server is shutting down')
+    finally:
+        server_socket.close()
 
 if __name__ == "__main__":
     main()
